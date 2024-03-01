a2109c961d7a41a841b230795433c7e8e8f71260
William Felton <williamfelton2006@icloud.com>
2024-03-01 14:29:05 +0000

    added square root functionality

--- Calculator.java
+++ Calculator.java
@@ -2,39 +2,46 @@ import java.util.Scanner;
 
 class Calculator{
     public static void main(String args[]) {
-        char operator;
+        String operator;
         float num1, num2, output;
+        double ans;
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter your first number: ");
+        System.out.println("Enter your first number (if root enter same number twice): ");
         num1 = input.nextFloat();
         System.out.println("Enter your second number: ");
         num2 = input.nextFloat();
-        System.out.println("Enter your operator: ");
-        operator = input.next().charAt(0);
+        System.out.println("Enter your operator (+,-,*,/,^,sqrt): ");
+        operator = input.next();
 
         switch(operator){
-            case '*':
+            case "*":
                 output = num1 * num2;
                 System.out.println("The answer is: " + output);
             break;
 
-            case '/':
+            case "/":
                 output = num1 / num2;
                 System.out.println("The answer is: " + output);
             break;
 
-            case '+':
+            case "+":
                 output = num1 + num2;
                 System.out.println("The answer is: " + output);
             break;
 
-            case '-':
+            case "-":
                 output = num1 - num2;
                 System.out.println("The answer is: " + output);
             break;
 
-            case '^':
-                double ans = Math.pow(num1, num2);
+            case "^":
+                ans = Math.pow(num1, num2);
+                System.out.println("The answer is: " + ans);
+            break;
+
+            case "root":
+            case "sqrt":
+                ans = Math.sqrt(num1);
                 System.out.println("The answer is: " + ans);
             break;
 
